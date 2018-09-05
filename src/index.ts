ce0504d5b9bcaa7b2ff8d4d723d1df6635c123b4
LeonK <leon_kuehne@ultimatesoftware.com>
2018-09-05 15:56:46 -0400

    reconfigured

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 export * from './lib/async';
 export * from './lib/hash';
 export * from './lib/number';
-
-async function initServer(): Promise<void> {
-  console.log('hello');
-}
-
-// tslint:disable-next-line:no-expression-statement
-initServer();
