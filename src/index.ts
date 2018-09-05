67c98aa83ed6e158b2b0caecd5fb029dba86f44a
LeonK <leon_kuehne@ultimatesoftware.com>
2018-09-05 13:16:51 -0400

    added start script and gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from './lib/async';
 export * from './lib/hash';
 export * from './lib/number';
+
+async function initServer(): Promise<void> {
+  console.log('hello');
+}
+
+// tslint:disable-next-line:no-expression-statement
+initServer();
